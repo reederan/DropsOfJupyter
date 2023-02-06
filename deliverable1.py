dad8fff063978471f4bd2ed6b6b803e11fd7bfd3
reederan <reederan@uw.edu>
2023-02-06 10:06:41 -0800

    Update deliverable1.py

--- deliverable1.py
+++ deliverable1.py
@@ -10,7 +10,7 @@ import pandas as pd
 ##loading in data 
 
 repo='https://github.com/reederan/DropsOfJupyter/raw/main/'
-csvfile1='annual-number-of-deaths-by-cause.csv'
+csvfile1='infectious-disease-death-rates.csv'
 csvfile2='distribution-human-rights-vdem.csv'
 csvfile3='water-and-sanitation.csv'
 deathscsv=pd.read_csv(repo + csvfile1)
@@ -18,4 +18,24 @@ humanrightscsv=pd.read_csv(repo + csvfile2)
 sanitationcsv=pd.read_csv(repo + csvfile3)
 
 ##cleaning and formatting
+
+
+deathscsv.columns
+humanrightscsv.columns
+sanitationcsv.columns
+
+deathscsv.head(3)
+humanrightscsv.head(3)
+sanitationcsv.head(3)
+
+###removing unnecessary columns 
+byedeaths=[]
+         #human rights csv
+byehr=[1,4]
+humanrightscsv.drop(columns=humanrightscsv.columns[bye],inplace=True)
+
+byesanitation=[]
+
+###narrowing down the dates to 2015-2019 
+
 
